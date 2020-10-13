69b8954a4c605361889690e0b43269a067aea157
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-13 16:46:49 +0100

    Users can post and view projects

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -19,7 +19,7 @@ class ProjectsTest extends TestCase
             'description' => $this->faker->paragraph,
         ];
 
-        $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         $this->assertDatabaseHas('projects', $attributes);
 
