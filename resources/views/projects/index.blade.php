749115beea430710a4729e51bca97ac55f07f356
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-19 16:01:58 +0100

    Layout for the show project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <header class="flex items-center mb-3 py-4">
-    <div class="flex justify-between items-center w-full">
+    <div class="flex justify-between items-end w-full">
     <h2 class="text-white text-xl">My projects</h2>
     <button href="/projects/create" class="button">New project</button>
     </div>
@@ -11,7 +11,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-2 pb-6">
-                <div class="bg-white rounded-lg shadow-sm p-5" style="height:200px">
+                <div class="card" style="height:200px">
             <h3 class="font-normal text-lg py-4 -ml-5 mb-3 border-l-4 border-gray-400 pl-4">
         <a href="{{$project->path()}}">{{$project->title}}</a>
             </h3>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$project->title}}</h1>
+    <header class="flex items-center mb-3 py-4">
+    <div class="flex justify-between items-end w-full">
+    <h2 class="text-white text-xl">My projects</h2>
+    <button href="/projects/create" class="button">New project</button>
+    </div>
+    </header>
 
-    <div>{{$project->description}}</div>
-    <a href="/projects">Go Back</a>
-@endsection
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+            <div class="mb-6">
+                <h2 class="text-white text-l mb-3">Tasks</h2>
+                    {{-- Tasks --}}
+                            <div class="card">
+                                 Lorem ipsum
+                                    </div>
+                                </div>
+                            <div>
+                        <h2 class="text-white text-l mb-3">Notes</h2>
+                            {{-- General notes --}}
+                                <div class="card">
+                                        Lorem ipsum
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="lg:w-1/4 px-3">
+                    <div class="card">
+                        <h1>{{$project->title}}</h1>
+                            <div>{{$project->description}}</div>
+                        <a href="/projects">Go Back</a>
+                    </div>
+                </div>
+                    </div>
+                        </main>
+                    @endsection
