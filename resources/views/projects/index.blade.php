08469da5ddc31126911d99ffd74b840843178d89
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-19 14:40:03 +0100

    Cards created for projects, title and logo to navbar

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,14 +4,17 @@
     <div class="flex items-center mb-3">
     <a href="/projects/create">Add a new project</a>
     </div>
-<ul>
-    @forelse($projects as $project)
 
-    <a href="{{$project->path()}}">
-    <li>{{ $project->title }}</li>
-    </a>
+<div class="flex flex-wrap -mx-3">
+    @forelse($projects as $project)
+    <div class="w-1/3 px-2 pb-6">
+        <div class="bg-white rounded shadow-sm p-5" style="height:200px">
+            <h3 class="font-normal text-lg py-4">{{$project->title}}</h3>
+            <div class="text-gray-500">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+        </div>
+    </div>
     @empty
-    <li>No projects yet</li>
-    @endforelse
-    </ul>
+        <div>No projects yet.</div>
+        @endforelse
+    </div>
 @endsection
