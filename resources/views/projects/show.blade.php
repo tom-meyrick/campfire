4c476fcbb4d6a8fbf323311946ed5374861cc93c
Tom Meyrick <tomgmeyrick@gmail.com>
2020-10-20 12:01:39 +0100

    Users can add tasks to projects from the project show page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,22 +16,29 @@
             <div class="mb-8">
                 <h2 class="text-white text-l mb-3">Tasks</h2>
                     {{-- Tasks --}}
-                    @foreach($project->tasks as $task)
-                            <div class="card">
+                    @forelse($project->tasks as $task)
+                            <div class="card mb-3">
                                  {{$task->body}}
+                                        </div>
+                                            @empty
+                                        @endforelse
+                                    <div class="card mb-3">
+                                <form action="{{ $project->path() . '/tasks'}}" method="POST">
+                            @csrf
+                        <input placeholder="Add a task..." class="w-full" name="body"/>
+                    </form>
+                </div>  
+            </div>
+                <div>
+                    <h2 class="text-white text-l mb-3">Notes</h2>
+                        {{-- General notes --}}
+                            <textarea class="card w-full" style="min-height: 200px">
+                                </textarea>
                                     </div>
-                    @endforeach
-                                </div>
-                            <div>
-                        <h2 class="text-white text-l mb-3">Notes</h2>
-                            {{-- General notes --}}
-                                <textarea class="card w-full" style="min-height: 200px">
-                                    </textarea>
-                                </div>
+                                        </div>
+                                    <div class="lg:w-1/4 px-3">
+                                @include('projects.card')
                             </div>
-                        <div class="lg:w-1/4 px-3">
-                    @include('projects.card')
-                </div>
-                    </div>
-                        </main>
-                    @endsection
+                        </div>
+                    </main>
+                @endsection
--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -32,7 +32,19 @@ class ProjectTasksTest extends TestCase
         $this->signIn();
         $project = Project::factory()->create(['owner_id' => auth()->id()]);
         $attributes = Task::factory()->raw(['body' => '']);
-        $this->post($project->path() . '/tasks', $attributes)->assertSessionHasErrors('body');
+        $this->post($project->path() . '/tasks', $attributes)
+             ->assertSessionHasErrors('body');
       }
+
+     /** @test */
+     public function only_the_owner_of_a_project_can_add_tasks()
+     {
+        $this->signIn(); 
+        $project = Project::factory()->create();
+        $this->post($project->path() . '/tasks', ['body' => 'Test task'])
+             ->assertStatus(403);
+
+        $this->assertDatabaseMissing('tasks', ['body' => 'Test task']);
+     }
 
 }
