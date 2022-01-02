091f87a54951fc1ab9b81ca264e642b27dcf8d14
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-02 15:41:14 +0000

    Created layout file for Blade templates and added Bootstrap cdn

--- campfire/resources/views/post.blade.php
+++ campfire/resources/views/post.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-</head>
-<body>
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-    <a href="/">Go back</a>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+<article>
+    <h1>
+        {{ $post->title }}
+    </h1>
+    <div>
+        {!! $post->body !!}
+    </div>
+</article>
+<a href="/">Go back</a>
+@endsection
--- campfire/resources/views/posts.blade.php
+++ campfire/resources/views/posts.blade.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <?php foreach($posts as $post) : ?>
-        <article>
+@section('content')
+    @foreach($posts as $post)
+    <article>
         <h1>
-            <a href="/posts/<?= $post->slug ?>">
+            <a href="/posts/{{ $post->slug }}">
                 {{ $post->title }}
             </a>
         </h1>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-        </article>
-        <?php endforeach; ?>
-    </body>
-</html>
+    <div>
+    {{ $post->excerpt }}
+    </div>
+            </article>
+        @endforeach
+@endsection
+
