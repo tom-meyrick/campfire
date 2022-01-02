9f28ac9a46e0670c63f91c04e7e6346dc72dc92d
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-02 16:03:37 +0000

    Added mt and mb to posts along with hr

--- campfire/resources/views/posts.blade.php
+++ campfire/resources/views/posts.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @foreach($posts as $post)
-    <article>
+    <article class="mt-5 mb-5">
         <h1>
             <a href="/posts/{{ $post->slug }}">
                 {{ $post->title }}
@@ -9,6 +9,7 @@
     <div>
     {{ $post->excerpt }}
     </div>
+    <hr/>
             </article>
         @endforeach
 </x-layout>
