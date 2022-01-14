55f2de84131030df8370e29447a2fb7da171eee9
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-14 09:47:09 +0000

    Switched to route model binding and referencing posts by slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach($posts as $post)
     <article class="mt-5 mb-5">
         <h1>
-            <a href="/posts/{{ $post->id }}">
+            <a href="/posts/{{ $post->slug }}">
                 {{ $post->title }}
             </a>
         </h1>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function($id) {
-    return view('post', ['post' => Post::findOrFail($id)]);
+Route::get('posts/{post:slug}', function(Post $post) {
+    return view('post', ['post' => $post]);
 });
