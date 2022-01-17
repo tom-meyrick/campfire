2b343a5b5d06dfb44010e27b0d6a8c0bd500ed5c
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-17 16:11:03 +0000

    Built factories for posts, users and categories and set up definition methods for each

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,52 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My first post', 
-            'slug' => 'my-first-post',
-            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi vel aliquid eos quibusdam accusantium quis reprehenderit ad, perferendis quisquam corrupti nemo libero quaerat nostrum maxime voluptatibus consequatur magnam culpa!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My second post', 
-            'slug' => 'my-second-post',
-            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi vel aliquid eos quibusdam accusantium quis reprehenderit ad, perferendis quisquam corrupti nemo libero quaerat nostrum maxime voluptatibus consequatur magnam culpa!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My third post', 
-            'slug' => 'my-third-post',
-            'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi vel aliquid eos quibusdam accusantium quis reprehenderit ad, perferendis quisquam corrupti nemo libero quaerat nostrum maxime voluptatibus consequatur magnam culpa!</p>'
-        ]);
+        $user = User::factory()->create(['name' => 'Tom Meyrick']);
+        Post::factory(6)->create(['user_id' => $user->id]);
     }
 }
