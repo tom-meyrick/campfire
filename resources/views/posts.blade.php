8d648de6d4fe280f78bb8003d330baf791866c7c
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-18 12:34:59 +0000

    Set up Blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @foreach($posts as $post)
+    {{-- @foreach($posts as $post)
     <article class="mt-5 mb-5">
         <h1>
             <a href="/posts/{{ $post->slug }}">
@@ -14,6 +14,23 @@
     </div>
     <hr/>
             </article>
-        @endforeach
+        @endforeach --}}
+
+        @include('_posts-header')
+        
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card />
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card />
+                <x-post-card />
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card />
+                <x-post-card />
+                <x-post-card />
+            </div>
+        </main>
 </x-layout>
 
