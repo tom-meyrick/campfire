8ca1ef456b7c9972b41f81f197ee5add8a420606
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-18 10:48:46 +0000

    Created protected $with method in Post model

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -31,13 +31,13 @@ Route::get('posts/{post:slug}', function(Post $post) {
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts
     ]);
 });
 
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts
     ]);
 });
 
