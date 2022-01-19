282fd15091a10ae2fbe18e075a774a5a9af44ff9
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-19 15:04:55 +0000

    Built drop down component to switch between categories

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,8 +2,10 @@
 
 <title>Laravel From Scratch Blog</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
