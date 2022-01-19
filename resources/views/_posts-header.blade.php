b15c44a3eeda45b665852dc8566df9e043d0c065
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-19 15:29:39 +0000

    Seperated dropdown menu into components and formatted blade files

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,8 +3,7 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="./images/lary-head.svg"
-                                                       alt="Head of Lary the mascot"></h2>
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="./images/lary-head.svg" alt="Head of Lary the mascot"></h2>
 
     <p class="text-sm mt-14">
         Another year. Another update. We're refreshing the popular Laravel series with new content.
@@ -14,33 +13,35 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
-            <div x-data="{ show: false, }">
-                <button @click="show = ! show" @click.away="show = false" class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w32 text-left flex lg:inline-flex">
-                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>
-                </button>
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display:none">
-                    <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a>
-
-                    @foreach($categories as $category)
-                    <a href="/categories/{{ $category->slug }}" class="block text-left px-3 
-                        text-sm leading-6 hover:bg-blue-500 
-                        focus:bg-blue-500 hover:text-white focus:text-white
-                        {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
-                        ">
+            <x-dropdown>
+                <x-slot name='trigger'>
+                    <button @click="show = ! show" @click.away="show = false"
+                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w32 text-left flex lg:inline-flex">
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                            height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                                </path>
+                                <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                            </g>
+                        </svg>
+                    </button>
+                </x-slot>
+                <a href="/"
+                    class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a>
+                @foreach ($categories as $category)
+                    <a href="/categories/{{ $category->slug }}"
+                        class="block text-left px-3 
+                text-sm leading-6 hover:bg-blue-500 
+                focus:bg-blue-500 hover:text-white focus:text-white
+                {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
+                ">
                         {{ ucwords($category->name) }}
                     </a>
-                    @endforeach
-                </div>
-            </div>
+                @endforeach
+            </x-dropdown>
         </div>
 
         <!-- Other Filters -->
@@ -54,13 +55,13 @@
                 </option>
             </select>
 
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
+            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22" height="22"
+                viewBox="0 0 22 22">
                 <g fill="none" fill-rule="evenodd">
                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                     </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                    <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                    </path>
                 </g>
             </svg>
         </div>
@@ -69,7 +70,7 @@
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm">
+                    class="bg-transparent placeholder-black font-semibold text-sm">
             </form>
         </div>
     </div>
