4147ad9f3b4125c9012a6d8ef8d941907ec36a0b
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-20 14:36:31 +0000

    Set up search function, built PostController and rerouted requests to search and posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,14 @@ class Post extends Model
     protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id']; 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters) {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%');
+        });
+    }
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
@@ -18,17 +19,9 @@ use Symfony\Component\Translation\Loader\YamlFileLoader;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' =>Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function(Post $post) {
-    return view('post', 
-    ['post' => $post]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
