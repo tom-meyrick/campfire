77201ac091b78125b5b41a52163b5f39250077f9
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-21 11:23:44 +0000

    Now passing category requests through PostController

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -20,7 +20,7 @@
                     All
                 </x-dropdown-item>
                 @foreach ($categories as $category)
-                    <x-dropdown-item href="/categories/{{ $category->slug }}"
+                    <x-dropdown-item href="/?category={{ $category->slug }}"
                         :active="request()->is('categories/' . $category->slug)">
                         {{ ucwords($category->name) }}
                     </x-dropdown-item>
