9054d7d69a7d273cc1c229cbea45ba56cd4dcf7c
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-25 14:35:21 +0000

    Added pagination and removed page from query string when searching by category

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -12,7 +12,7 @@
                 All
             </x-dropdown-item>
             @foreach ($categories as $category)
-                <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+                <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
                     :active="request()->is('categories/' . $category->slug)">
                     {{ ucwords($category->name) }}
                 </x-dropdown-item>
