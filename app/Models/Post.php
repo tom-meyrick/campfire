0108282873f413f78ba5a093fc3ba5b9c210c53a
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-25 13:58:55 +0000

    Fixed orWhere bug that was causing return of irrelevant post when searching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,11 +14,13 @@ class Post extends Model
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters) {
-        $query->when($filters['search'] ?? false, function ($query, $search) {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+          $query->where(fn($query) =>
             $query
             ->where('title', 'like', '%' . $search . '%')
-            ->orWhere('body', 'like', '%' . $search . '%');
-        });
+            ->orWhere('body', 'like', '%' . $search . '%')
+          )
+        );
 
             $query->when($filters['category'] ?? false, fn($query, $category) => 
             $query->whereHas('category', fn($query) => 
