e25c027f9d994aef50ee3eb5bf44cef948ec5e71
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-26 13:12:19 +0000

    Built login page and connected to SessionsController

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,29 @@ namespace App\Http\Controllers;
 
 class SessionsController extends Controller
 {
+
+    public function create() {
+        return view('sessions.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome back');
+        }
+
+        return back()
+                ->withInput()
+                ->withErrors([
+                    'password' => 'Your email or password is incorrect - please try again',
+                ]);
+    }
+
     public function destroy() {
         auth()->logout();
 
