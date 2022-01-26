a673f3d21b03d059689b740e10e584031a0d2450
Tom Meyrick <tomgmeyrick@gmail.com>
2022-01-26 12:46:06 +0000

    Setup logout and login routes

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SessionsController extends Controller
+{
+    public function destroy() {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'See you soon!');
+    }
+}
