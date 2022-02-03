3699250fcb52f9857741233d45ecd3b2f1e838c0
Tom Meyrick <tomgmeyrick@gmail.com>
2022-02-03 14:23:20 +0000

    Edited register form to use components

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,64 +1,19 @@
 <x-layout>
     <section class="px-6 py8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 p-6 rounded-xl border border-gray-200">
-            <h1 class="text-center font-bold text-xl">Register</h1>
-            <form method="POST" action="/register" class="mt-10">
-                @csrf
-                {{-- Name --}}
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase" for="name">
-                        Name
-                    </label>
-                    <input class="w-full p-2 border border-gray-400 rounded" type="text" id="name" name="name"
-                        value="{{ old('name') }}" required>
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                {{-- Username --}}
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase" for="username">
-                        Username
-                    </label>
-                    <input class="w-full p-2 border border-gray-400 rounded" type="text" id="username" name="username"
-                        value="{{ old('username') }}" required>
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                {{-- Email --}}
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase" for="email">
-                        Email
-                    </label>
-                    <input class="w-full p-2 border border-gray-400 rounded" type="email" id="email" name="email"
-                        value="{{ old('email') }}" required>
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                {{-- Password --}}
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase" for="password">
-                        Password
-                    </label>
-                    <input class="w-full p-2 border border-gray-400 rounded" type="password" id="password"
-                        name="password" required>
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                {{-- Button --}}
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register</h1>
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
+                    <x-form.input name="name" type="text" />
+                    <x-form.input name="username" type="text" />
+                    <x-form.input name="email" type="email" />
+                    <x-form.input name="password" type="password" />
+                    <x-form.button>
                         Submit
-                    </button>
-                </div>
-            </form>
+                    </x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
